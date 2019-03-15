3b6a7931a42be492335f166a3598c6f213c46c2f
Bruno Cangussu <bruno.cangussu@hotmail.com>
2019-03-14 22:08:16 -0300

    Calling main to actually work

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ const main = async () => {
     const gl = canvas.getContext('webgl');
     if(!gl) return;
     gl.clearColor(0.0,0.0,0.0,1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT)
-    gl.vertexAttrib1f(2, 20);
+    gl.clear(gl.COLOR_BUFFER_BIT);
 }
+
+main();
