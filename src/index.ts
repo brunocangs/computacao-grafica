9815f31c789cdd6e2247bae957adff080666156c
Bruno Cangussu <bruno.cangussu@hotmail.com>
2019-03-19 21:15:49 -0300

    Changes

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,26 @@
+import * as THREE from "three";
+import {axes, cube} from "./utils";
 
-import * as THREE from 'three';
-import {axes} from './utils'
 const width = window.innerWidth;
 const height = window.innerHeight;
 const axisWidth = width / 2;
 const axisHeight = height / 2;
-
 let renderer = new THREE.WebGLRenderer();
-renderer.setSize( width, height );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(width, height);
+document.body.appendChild(renderer.domElement);
 
-let camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
-camera.position.set( 0, 0, 100 );
-camera.lookAt( 0, 0, 0 );
+let camera = new THREE.OrthographicCamera(
+  width / -2,
+  width / 2,
+  height / 2,
+  height / -2,
+  1,
+  1000
+);
+camera.position.set(0, 0, 100);
+camera.lookAt(0, 0, 0);
 
 let scene = new THREE.Scene();
-
-
-
-scene.add( axes );
-renderer.render( scene, camera );
+scene.add(cube(100, 0xffffff));
+scene.add(axes());
+renderer.render(scene, camera);
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,18 +1,30 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
 const width = window.innerWidth;
 const height = window.innerHeight;
 const axisWidth = width / 2;
 const axisHeight = height / 2;
 
-export const axes = (() => {
-  let material = new THREE.LineBasicMaterial( { color: 0xffffff } );
+export const axes = () => {
+  let material = new THREE.LineBasicMaterial({ color: 0xffffff });
   let axes = new THREE.Geometry();
-  axes.vertices.push(new THREE.Vector3( -axisWidth, 0, 0) );
-  axes.vertices.push(new THREE.Vector3( axisWidth, 0, 0) );
-  axes.vertices.push(new THREE.Vector3( 0, 0, 0) );
-  axes.vertices.push(new THREE.Vector3( 0, -axisHeight, 0) );
-  axes.vertices.push(new THREE.Vector3( 0, axisHeight, 0) );
-  let line = new THREE.Line( axes, material );
+  axes.vertices.push(new THREE.Vector3(-axisWidth, 0, 0));
+  axes.vertices.push(new THREE.Vector3(axisWidth, 0, 0));
+  axes.vertices.push(new THREE.Vector3(0, 0, 0));
+  axes.vertices.push(new THREE.Vector3(0, -axisHeight, 0));
+  axes.vertices.push(new THREE.Vector3(0, axisHeight, 0));
+  let line = new THREE.Line(axes, material);
   return line;
-})();
+};
+
+export const cube = (size: number, color: string | number | THREE.Color) => {
+  var cube = new THREE.BoxBufferGeometry(size, size, size);
+  cube.translate(10, 10, 0);
+  let mesh = new THREE.Mesh(
+    cube,
+    new THREE.MeshBasicMaterial({
+      color
+    })
+  );
+  return mesh;
+};
